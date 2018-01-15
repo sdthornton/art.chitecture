752e496b1cb2b2fec2fe7c48c041c6ba40598264
Sam Thornton <sdthornton@live.com>
2018-01-15 02:01:06 -0800

    Add about page. Change out images.

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,18 @@
+class ProjectsController < ApplicationController
+
+  def index
+  end
+
+  def contemporary
+  end
+
+  def french_country
+  end
+
+  def modern_mediterranean
+  end
+
+  def nw_traditional
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,14 @@ Rails.application.routes.draw do
 
   root to: 'home#index'
 
+  resources :projects, only: :index
+  get 'projects/contemporary' => 'projects#contemporary', as: 'contemporary_project'
+  get 'projects/french_country' => 'projects#french_country', as: 'french_country_project'
+  get 'projects/modern_mediterranean' => 'projects#modern_mediterranean', as: 'modern_mediterranean_project'
+  get '/projects/nw_traditional' => 'projects#nw_traditional', as: 'nw_traditional_project'
   resources :services, only: :index
   get '/process' => 'process#index', as: 'process'
   get '/contact' => 'contact#index', as: 'contact'
+  get '/about' => 'about#index', as: 'about'
 
 end
